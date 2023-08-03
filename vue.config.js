9a809b6dd6c0b5e7200254b8fcd12178afdc518f
claudio <claudio.inghingolo@gmail.com>
2023-08-03 17:16:17 +0200

    Add deployment files

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://github.com/claudio-ing/weather-app'
+    : '/',
 
   pluginOptions: {
     vuetify: {
